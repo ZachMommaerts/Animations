08ebee357e39266a7ea0314dd8d4b568e809e0c1
ZachMommaerts <zmommaerts@gmail.com>
2023-09-14 16:04:36 -0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Zach Mommaerts on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
